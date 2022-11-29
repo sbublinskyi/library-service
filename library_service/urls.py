e1c96262eebf94663c431ca11c844941319d010e
Yevhenii Davydenko <111540421+zhulich@users.noreply.github.com>
2022-11-29 16:33:14 +0200

    Revert "Implement crud functionality for books service"

--- library_service/urls.py
+++ library_service/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -34,5 +34,4 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path("api/", include("books.urls", namespace="books"))
 ]
