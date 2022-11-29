8e4700489578865de6201f411aa4c901e7df19c2
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-29 13:56:51 +0200

    Create model Book and func validate_positive

--- books/models.py
+++ books/models.py
@@ -1,3 +1,25 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+def validate_positive(value):
+    if value < 0:
+        raise ValidationError(
+            _(f"{value} is not a positive number"),
+            params={"value": value},
+        )
+
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "H", _("Hard")
+        SOFT = "S", _("Soft")
+    title = models.CharField(max_length=63, unique=True)
+    author = models.CharField(max_length=63)
+    cover = models.CharField(choices=Cover.choices)
+    inventory = models.IntegerField(validators=[validate_positive])
+    daily_fee = models.DecimalField(validators=[validate_positive])
+
+    def __str__(self):
+        return self.title
