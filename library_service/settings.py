3e919b95da6a3082e6c1781c173fd7dd3fbf1fc4
Ivan Antonishak <jus1stored@gmail.com>
2022-11-29 17:20:47 +0200

    Create user serializer and install JWT

--- library_service/settings.py
+++ library_service/settings.py
@@ -124,6 +124,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
 
 SPECTACULAR_SETTINGS = {
