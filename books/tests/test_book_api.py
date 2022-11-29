2fe419bb5e387ed9a5bcadb3408297757dcbb102
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-30 01:24:21 +0200

    Create test for book api

---
+++ books/tests/test_book_api.py
@@ -0,0 +1,155 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from books.models import Book
+from books.serializers import BookSerializer
+
+BOOK_URL = reverse("books:book-list")
+PAYLOAD = {
+    "title": "Payload book",
+    "author": "Second Last",
+    "cover": "H",
+    "inventory": 45,
+    "daily_fee": 10.25,
+}
+
+
+def sample_book(**params):
+    defaults = {
+        "title": "Test book",
+        "author": "First Last",
+        "cover": "S",
+        "inventory": 50,
+        "daily_fee": 15.50,
+    }
+    defaults.update(params)
+
+    return Book.objects.create(**defaults)
+
+
+def detail_url(book_id):
+    return reverse("books:book-detail", args=[book_id])
+
+
+class UnauthenticatedBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_list_book(self):
+        sample_book(title="Test1")
+        sample_book(title="Test2")
+
+        res = self.client.get(BOOK_URL)
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_retrieve_movie_detail(self):
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.get(url)
+
+        serializer = BookSerializer(book)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_book_forbidden(self):
+
+        res = self.client.post(BOOK_URL, PAYLOAD)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_book_forbidden(self):
+
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.put(url, PAYLOAD)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_book_forbidden(self):
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AuthenticatedBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpassword",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_books(self):
+        sample_book(title="Test1")
+        sample_book(title="Test2")
+
+        res = self.client.get(BOOK_URL)
+
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_book_forbidden(self):
+
+        res = self.client.post(BOOK_URL, PAYLOAD)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_book_forbidden(self):
+
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.put(url, PAYLOAD)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_book_forbidden(self):
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_superuser(
+            "admin@admin.com", "testpassword", is_staff=True, is_superuser=True
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_create_book(self):
+
+        res = self.client.post(BOOK_URL, PAYLOAD)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        book = Book.objects.get(id=res.data["id"])
+        for key in PAYLOAD.keys():
+            self.assertEqual(PAYLOAD[key], getattr(book, key))
+
+    def test_update_book(self):
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.put(url, PAYLOAD)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_delete_book_(self):
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
