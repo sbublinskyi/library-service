704dd6f9f1e5fc397923bbac0a98cf77dca8b23e
Kitazuka <ilya.artnaev@gmail.com>
2022-12-01 11:00:29 +0200

    Implement simple succes action

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,15 @@
+import stripe
+from django.http import HttpResponse
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 
 from payments.models import Payment
-from payments.serializers import PaymentListSerializer, PaymentDetailSerializer
+from payments.serializers import (
+    PaymentListSerializer,
+    PaymentDetailSerializer,
+    PaymentSuccessSerializer
+)
 
 
 class PaymentViewSet(
@@ -25,3 +32,16 @@ class PaymentViewSet(
             return PaymentListSerializer
         if self.action == "retrieve":
             return PaymentDetailSerializer
+        if self.action == "success":
+            return PaymentSuccessSerializer
+
+    @action(methods=["GET"], url_path="success", detail=False)
+    def return_book(self, request, session_id=None):
+        session_id = request.query_params.get("session_id")
+        payment = Payment.objects.get(session_id=session_id)
+        payment.status = "PAID"
+        payment.type = "FINE"
+        payment.save()
+        session = stripe.checkout.Session.retrieve(session_id)
+
+        return HttpResponse('<html><body><h1>Thanks for your order, customer.name!</h1></body></html>')
