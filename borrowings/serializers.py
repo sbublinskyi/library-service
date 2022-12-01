f4885f2eb69dce3d9b0070e459b0be15bb253af1
Ivan Antonishak <jus1stored@gmail.com>
2022-12-01 14:05:22 +0200

    Payment create with new borrowing

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,13 +1,41 @@
 import datetime
+import os
 
+import stripe
 from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+from books.models import Book
 from books.serializers import BookSerializer
 from borrowings import telegram_bot
 from borrowings.models import Borrowing
+from payments.models import Payment
 from user.serializers import UserSerializer
+
+
+stripe.api_key = os.environ.get("API_KEY")
+
+
+def create_stripe_session(book: Book, price: float) -> tuple:
+    session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {
+                        "name": book.title,
+                    },
+                    "unit_amount": int(price * 100),
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url="http://localhost:8000/success",
+        cancel_url="http://localhost:8000/cancel",
+    )
+    return session.url, session.id
 
 
 class BorrowingCreateSerializer(serializers.ModelSerializer):
@@ -48,13 +76,24 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
                 f"User: {validated_data['user']}\n"
                 f"Book: {validated_data['book']}\n"
             )
-
-            return Borrowing.objects.create(**validated_data)
+            borrowing = Borrowing.objects.create(**validated_data)
+            money_to_pay = book.daily_fee * Borrowing.MAX_TERM
+            session_url, session_id = create_stripe_session(book, money_to_pay)
+            Payment.objects.create(
+                status="PENDING",
+                borrowing=borrowing,
+                type="PAYMENT",
+                session_url=session_url,
+                session_id=session_id,
+                money_to_pay=money_to_pay,
+            )
+            return borrowing
 
 
 class BorrowingListSerializer(serializers.ModelSerializer):
     book_title = serializers.CharField(source="book.title", read_only=True)
     user_email = serializers.CharField(source="user.email", read_only=True)
+    payment_url = serializers.URLField(source="payment.session_url")
 
     class Meta:
         model = Borrowing
@@ -65,12 +104,19 @@ class BorrowingListSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "book_title",
             "user_email",
+            "payment_url",
         )
 
 
 class BorrowingDetailSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False)
     book = BookSerializer(many=False)
+    payment_url = serializers.URLField(source="payment.session_url")
+    payment_id = serializers.CharField(source="payment.session_id")
+    payment_status = serializers.CharField(source="payment.status")
+    money_to_pay = serializers.DecimalField(
+        source="payment.money_to_pay", max_digits=5, decimal_places=2
+    )
 
     class Meta:
         model = Borrowing
@@ -81,6 +127,10 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "user",
             "book",
+            "payment_status",
+            "payment_url",
+            "payment_id",
+            "money_to_pay",
         )
 
 
--- payments/serializers.py
+++ payments/serializers.py
@@ -1,39 +1,12 @@
-import os
-
-import stripe
-from django.db import transaction
 from rest_framework import serializers
 
-from books.models import Book
-from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingDetailSerializer
 from payments.models import Payment
 
-stripe.api_key = os.environ.get("API_KEY")
-
-
-def create_stripe_session(book: Book, price: float) -> tuple:
-    session = stripe.checkout.Session.create(
-        line_items=[
-            {
-                "price_data": {
-                    "currency": "usd",
-                    "product_data": {
-                        "name": book.title,
-                    },
-                    "unit_amount": int(price * 100),
-                },
-                "quantity": 1,
-            }
-        ],
-        mode="payment",
-        success_url="http://localhost:8000/success",
-        cancel_url="http://localhost:8000/cancel",
-    )
-    return session.url, session.id
-
 
 class PaymentListSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source="borrowing.user.email")
+
     class Meta:
         model = Payment
         fields = (
@@ -44,27 +17,8 @@ class PaymentListSerializer(serializers.ModelSerializer):
             "session_url",
             "session_id",
             "money_to_pay",
+            "user",
         )
-        read_only_fields = (
-            "money_to_pay",
-            "session_url",
-            "session_id",
-        )
-
-    def create(self, validated_data):
-        with transaction.atomic():
-            borrowing = validated_data["borrowing"]
-            book = borrowing.book
-            money_to_pay = book.daily_fee * Borrowing.MAX_TERM
-            session_url, session_id = create_stripe_session(book, money_to_pay)
-            return Payment.objects.create(
-                status=validated_data["status"],
-                borrowing=borrowing,
-                type=validated_data["type"],
-                session_url=session_url,
-                session_id=session_id,
-                money_to_pay=money_to_pay,
-            )
 
 
 class PaymentDetailSerializer(serializers.ModelSerializer):
